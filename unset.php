a001d0188bcdf4bdb7db99a3605dd7826493d68b
Andrii Kalmus <andrii.kalmus@abysim.com>
2026-02-03 00:31:53 +0200

    Add secret check to unset.php to ensure security

--- unset.php
+++ unset.php
@@ -1,16 +1,6 @@
 <?php
 
 /**
- * This file is part of the PHP Telegram Bot example-bot package.
- * https://github.com/php-telegram-bot/example-bot/
- *
- * (c) PHP Telegram Bot Team
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-/**
  * This file is used to unset / delete the webhook.
  */
 
@@ -20,6 +10,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 // Load all configuration options
 /** @var array $config */
 $config = require __DIR__ . '/config.php';
+
+// Secret check - must pass ?secret=xxx
+if (!isset($_GET['secret']) || $_GET['secret'] !== $config['secret']) {
+    http_response_code(403);
+    die('Forbidden');
+}
 
 try {
     // Create Telegram API object
