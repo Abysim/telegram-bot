c83169522d3bc9ba47124bd6647923a1ee1e2660
Abysim <abysim@whitelion.me>
2026-03-06 03:13:49 +0200

    Auto cleanup new tables

--- Commands/Admin/CustomcleanupCommand.php
+++ Commands/Admin/CustomcleanupCommand.php
@@ -251,6 +251,9 @@ class CustomcleanupCommand extends AdminCommand
             'poll'            => ['table' => TB_POLL, 'field' => 'created_at'],
             'request_limiter' => ['table' => TB_REQUEST_LIMITER, 'field' => 'created_at'],
             'shipping_query'  => ['table' => TB_SHIPPING_QUERY, 'field' => 'created_at'],
+            'chat_join_request'  => ['table' => TB_CHAT_JOIN_REQUEST, 'field' => 'created_at'],
+            'message_reaction_count'  => ['table' => TB_MESSAGE_REACTION_COUNT, 'field' => 'created_at'],
+            'message_reaction'  => ['table' => TB_MESSAGE_REACTION, 'field' => 'created_at'],
         ];
 
         foreach (array_intersect(array_keys($simple_tables), $tables_to_clean) as $table_to_clean) {
